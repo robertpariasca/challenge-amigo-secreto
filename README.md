0fd66cefbb44192fc42ee23ffe1eb9d41069ad4c
robertpariasca <robertpariasca34@gmail.com>
2025-08-14 23:50:17 -0500

    Actualizacion de Readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 Challenge 1
+
+Proyecto de Amigo Secreto
+
+Se pidió crear una interfaz que permita ingresar varios nombres, mostrarlos, y al presionar un boton, elegír uno de todos los ingresados y mostrarlo en la pantalla.
+
+Para este proyecto se tienen 3 funciones principales:
+
+agregarAmigo: Valida que se haya ingresado un dato en el campo respectivo, y que este no se repita. En caso cumpla las condiciones, agrega el nombre a la lista y llama a la siguiente funcion
+
+    En caso el campo este vacío mostrará el error "Debe ingresar un nombre".
+
+    En caso el nombre esté repetido mostrará el error "Ya ingresó este nombre previamente".
+
+    En caso tenga exito, ingresará el valor a la lista y llamará a la funcion "mostrarAmigo".
+
+mostrarAmigo: Inserta la lista de amigos en un item HTML para mostrarlo en la pantalla.
+
+sortearAmigo: En caso haya al menos 2 personas en la lista, elegirá al azar uno de los nombres en la lista, y lo mostrará en color verde.
+
+    En caso haya 1 o ninguna persona, mostrará el error "Debe ingresar al menos 2 personas para sortear"
+
+    En caso haya 2 o mas personas, elegirá una.
--- app.js
+++ app.js
@@ -1,1 +1,59 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let arrayamigos = [];
+
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    let mostrarAmigoSeleccionado = document.getElementById('resultado');
+    mostrarAmigoSeleccionado.innerHTML = '';
+
+    if (nombreAmigo == '') {
+        alert('Debe ingresar un nombre');
+    }
+    else
+    {
+        if (arrayamigos.includes(nombreAmigo)) {
+            alert('Ya ingresó este nombre previamente');
+        } else {
+            arrayamigos.push(nombreAmigo);
+        }
+    
+    }
+    document.querySelector('#amigo').value = '';
+    //console.log(arrayamigos);
+    mostrarAmigo();
+    return;
+}
+
+function mostrarAmigo() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+    
+    for (let i = 0; i < arrayamigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = arrayamigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    let mostrarAmigoSeleccionado = document.getElementById('resultado');
+    let listaAmigos = document.getElementById('listaAmigos');
+    mostrarAmigoSeleccionado.innerHTML = '';
+    if (arrayamigos.length < 2)
+    {
+        alert('Debe ingresar al menos 2 personas para sortear');
+    }
+    else
+    {
+        let numeroSeleccionado =  Math.floor(Math.random()*arrayamigos.length);
+        let amigoSeleccionado = arrayamigos[numeroSeleccionado];
+
+
+        mostrarAmigoSeleccionado.innerHTML = amigoSeleccionado;
+
+        arrayamigos.length = 0;
+        listaAmigos.innerHTML = '';
+    }
+}
